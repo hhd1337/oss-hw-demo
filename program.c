996fa5c9c083544fdb70b33268cca2f88453c679
Alice <alice@example.com>
2025-11-27 08:01:32 +0000

    feat: 프로그램 버전 문구를 v4로 수정

--- program.c
+++ program.c
@@ -17,7 +17,7 @@ int add_even(int n) {
 }
 
 int main(void) {
-    printf("Open Source Software HW - version 3\n");
+    printf("Open Source Software HW - version 4\n");
     printf("Sum 1..10 = %d\n", add(10));
     printf("Even sum 1..10 = %d\n", add_even(10));
     return 0;
