55e0f4d74716e283608d7a494393dae1e1e18c8f
Alice <alice@example.com>
2025-11-27 07:40:38 +0000

    feat: 짝수 합을 계산하는 add_even 함수 추가

--- program.c
+++ program.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+int add(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+int add_even(int n) {
+    int sum = 0;
+    for (int i = 2; i <= n; i += 2) {
+        sum += i;
+    }
+    return sum;
+}
+
 int main(void) {
-    printf("Open Source Software HW - version 1\n");
+    printf("Open Source Software HW - version 3\n");
+    printf("Sum 1..10 = %d\n", add(10));
+    printf("Even sum 1..10 = %d\n", add_even(10));
     return 0;
 }
 
